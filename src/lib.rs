b0c7db3395660f0fb751c112e9ac60def80eaba7
Ruud van Asseldonk <dev@veniogames.com>
2015-01-17 21:53:52 +0100

    update crate metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Transducers -- Transducer library for Rust
+// Transducers -- A transducer library for Rust
 // Copyright (C) 2014-2015 Ruud van Asseldonk
 //
 // This program is free software: you can redistribute it and/or modify
