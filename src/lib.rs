785d9970e355b3bf8c08e767ffddea291965b153
Ruud van Asseldonk <dev@veniogames.com>
2015-01-19 22:07:26 +0100

    add compose function

--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,61 @@ pub fn transduce<'t, T, U, I: Iterator<Item = U>,
     state
 }
 
+/// The function composition `F` after `G`.
+pub struct Composed<X, Y, Z, F, G> {
+    f: F,
+    g: G
+}
+
+impl<X, Y, Z, F, G> Fn(X) -> Z for Composed<X, Y, Z, F, G>
+where F: Fn(Y) -> Z,
+      G: Fn(X) -> Y {
+    extern "rust-call" fn call(&self, arg: (X,)) -> Z {
+        let (x,) = arg;
+        let y = (self.g)(x);
+        let z = (self.f)(y);
+        z
+    }
+}
+
+/// Composes the functions `f` and `g` to the function `f` after `g`.
+pub fn compose<X, Y, Z, F, G>(f: F, g: G) -> Composed<X, Y, Z, F, G>
+where F: Fn(Y) -> Z,
+      G: Fn(X) -> Y {
+    Composed { f: f, g: g }
+}
+
+#[test]
+fn compose_is_associative() {
+    let f = |&: x: i32| x * 2;
+    let g = |&: x: i32| x + 2;
+    let h = |&: x: i32| x * x;
+    assert_eq!(compose(compose(f, g), h)(42), 3532);
+    let f = |&: x: i32| x * 2;
+    let g = |&: x: i32| x + 2;
+    let h = |&: x: i32| x * x;
+    assert_eq!(compose(f, compose(g, h))(42), 3532);
+}
+
+#[test]
+fn compose_typechecks() {
+    use std::num;
+    let f = |&: x: Option<i16>| if let Some(n) = x { n } else { 0 };
+    let g = |&: x: u16| num::cast::<u16, i16>(x);
+    let h = compose(f, g);
+    assert_eq!(h(42), 42);
+    assert_eq!(h(65535), 0);
+}
+
+#[test]
+fn compose_with_id_is_id() {
+    let id = |&: x: i32| x;
+    let ff = |&: x: i32| x * 2;
+    assert_eq!(ff(42), compose(id, ff)(42));
+    let id = |&: x: i32| x;
+    let ff = |&: x: i32| x * 2;
+    assert_eq!(ff(42), compose(ff, id)(42));
+}
 
 #[test]
 fn mapping_on_iter() {
